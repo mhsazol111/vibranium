088b4be0b5cd1fa4aee8fb49310146d927e1ac8a
SAZOL <mhsazol111@gmail.com>
2018-09-01 18:15:12 +0600

    including first file from desktop
    Signed-off-by: SAZOL <mhsazol111@gmail.com>

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+
+
+
+// function vibranium_scripts() {
+//     wp_enqueue_style( 'vibranium_style', get_stylesheet_uri() );
+// }
+// add_action( 'wp_enqueue_scripts', 'vibranium_scripts' );
